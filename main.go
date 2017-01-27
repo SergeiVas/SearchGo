7d0697f2e0e50004543aa67282a143a3a1741a6b
SergeiVas <shot.kolt@mail.ru>
2017-01-27 20:53:35 +0700

    add gorutin limit

--- main.go
+++ main.go
@@ -9,56 +9,67 @@ import (
 )
 
 func main() {
-	var(
-		countAddres int	 // Количество url'ов
-		AllUrl string	 // Строка со всеми Url'ами
-	//	gorutines int
-	//	k = 5
+	var (
+		countAddres int    // Количество url'ов
+		AllUrl      string // Строка со всеми Url'ами
 	)
-	ch := make(chan string) // Канал, в котором хранятся url
+	k := make(chan bool, 5)
+	ch := make(chan string)     // Канал, в котором хранятся url
 	countUrls := make(chan int) // Канал, в котором хранятся количство строк go на url'е
 	fmt.Scanln(&AllUrl)
-	go func(){
+	go func() {
 		countAddres = readData(ch, AllUrl)
 	}()
 	for num := range ch {
-			countStrings(num, countUrls)
+		go countStrings(num, countUrls, k)//, &wg)
 	}
 	count := 0
 	// Считываем количество строк go с каждого url'а
-	for i := range countUrls{
-			count += i
-			countAddres--
-			if countAddres <= 0{
-				break
-			}
+	for i := range countUrls {
+		count += i
+		countAddres--
+		if countAddres <= 0 {
+			break
 		}
+	}
 	fmt.Print("Total: ")
 	fmt.Println(count)
 
 }
 
 //Переходим по url'у и считаем кол - во искомых строк
-func countStrings(url string, c chan int) {
+func countStrings(url string, c chan int, k chan bool){ //, wg *sync.WaitGroup) {
+	k <- true
 	response, err := http.Get(url)
 	if err != nil {
 		fmt.Println("You can not go to url")
 	} else {
 		defer response.Body.Close()
-		s, _ := ioutil.ReadAll(response.Body)
+		s, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Println("Error 218")
+		} else {
 		count := strings.Count(string(s), "Go")
-		fmt.Print("Count for "+string(url)+":")
+		fmt.Print("Count for " + url + ":")
 		fmt.Println(count)
 		c <- count
+		err := <- k
+		if err == false{
+
+		}
+		
+		}
 	}
+
 }
 
 // Разбиваем строку на отдельные url'ы и отправляем их в канал
-func readData(c chan string, AllURL string) int{
+func readData(c chan string, AllURL string) int {
 	s := ""
 	countAddres := 0
 	// Считываем посимвольно строку пока не встретим "/n" и отправляем в канал
 	for i := 0; i < len(AllURL); i++ {
+		// пока не встретим "/n"
 		if AllURL[i] != 92 {
 			s += string(AllURL[i])
 		} else {
